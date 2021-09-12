937f2589c818b1ae025aa37ad4c64e8edd4ec87a
luzilong1993 <942146559@qq.com>
2021-09-12 14:00:07 +0800

    feat:react样式

---
+++ 04-样式/demo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>aaa</div>
+         );
+    }
+}
+ 
+export default App;
